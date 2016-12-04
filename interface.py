bd3e807d2458de2f5dc160c14fc1bac96467c3b0
Nichodon <brandonsyc.2222@yahoo.com>
2016-12-04 08:10:40 -0800

    Version 0.4

--- interface.py
+++ interface.py
@@ -50,6 +50,13 @@ def open_file():
         lines = eval(filer.read())
 
         if tkMessageBox.askyesno("Are you sure?", "All unsaved changes will be lost"):
+
+            for i in range(0, len(statements), 1):
+                line_nums[i].grid_forget()
+                statements[i].grid_forget()
+                reasons[i].grid_forget()
+                refs[i].grid_forget()
+
             statements = []
             reasons = []
             line_nums = []
@@ -58,19 +65,19 @@ def open_file():
             for i in range(0, len(lines), 1):
                 line_data = lines[i]
 
-                m = Label(tk, text=str(len(statements)+1)+".", width=5)
-                m.grid(row=len(statements)+6, column=0)
+                m = Label(f3, text=str(len(statements)+1)+".", width=5)
+                m.grid(row=len(statements), column=0)
 
-                en1 = Entry(tk, width=50)
-                en1.grid(row=len(statements)+6, column=1, columnspan=3)
+                en1 = Entry(f3, width=50)
+                en1.grid(row=len(statements), column=1, columnspan=3)
                 en1.insert(END, line_data[0])
 
-                en2 = Entry(tk, width=50)
-                en2.grid(row=len(statements)+6, column=4, columnspan=3)
+                en2 = Entry(f3, width=50)
+                en2.grid(row=len(statements), column=4, columnspan=3)
                 en2.insert(END, line_data[1])
 
-                sb = Entry(tk, width=10)
-                sb.grid(row=len(statements)+6, column=7)
+                sb = Entry(f3, width=10)
+                sb.grid(row=len(statements), column=7)
                 sb.insert(END, line_data[2])
 
                 line_nums.append(m)
@@ -85,17 +92,17 @@ def open_file():
 
 
 def insert():
-    m = Label(tk, text=str(len(statements)+1)+".", width=5)
-    m.grid(row=len(statements)+6, column=0)
+    m = Label(f3, text=str(len(statements)+1)+".", width=5)
+    m.grid(row=len(statements), column=0)
 
-    en1 = Entry(tk, width=50)
-    en1.grid(row=len(statements)+6, column=1, columnspan=3)
+    en1 = Entry(f3, width=50)
+    en1.grid(row=len(statements), column=1, columnspan=3)
 
-    en2 = Entry(tk, width=50)
-    en2.grid(row=len(statements)+6, column=4, columnspan=3)
+    en2 = Entry(f3, width=50)
+    en2.grid(row=len(statements), column=4, columnspan=3)
 
-    sb = Entry(tk, width=10)
-    sb.grid(row=len(statements)+6, column=7)
+    sb = Entry(f3, width=10)
+    sb.grid(row=len(statements), column=7)
 
     line_nums.append(m)
     statements.append(en1)
@@ -104,51 +111,51 @@ def insert():
 
 
 def insert_at():
-    m = Label(tk, text=str(len(statements)+1)+".", width=5)
-    m.grid(row=len(statements)+6, column=0)
+    m = Label(f3, text=str(len(statements)+1)+".", width=5)
+    m.grid(row=len(statements), column=0)
 
-    en1 = Entry(tk, width=50)
-    en1.grid(row=int(q.get())+5, column=1, columnspan=3)
+    en1 = Entry(f3, width=50)
+    en1.grid(row=int(p1.get())-1, column=1, columnspan=3)
 
-    en2 = Entry(tk, width=50)
-    en2.grid(row=int(q.get())+5, column=4, columnspan=3)
+    en2 = Entry(f3, width=50)
+    en2.grid(row=int(p1.get())-1, column=4, columnspan=3)
 
-    sb = Entry(tk, width=10)
-    sb.grid(row=int(q.get())+5, column=7)
+    sb = Entry(f3, width=10)
+    sb.grid(row=int(p1.get())-1, column=7)
 
     line_nums.append(m)
-    statements.insert(int(q.get()) - 1, en1)
-    reasons.insert(int(q.get()) - 1, en2)
-    refs.insert(int(q.get()) - 1, sb)
+    statements.insert(int(p1.get()) - 1, en1)
+    reasons.insert(int(p1.get()) - 1, en2)
+    refs.insert(int(p1.get()) - 1, sb)
 
     for i in range(0, len(statements), 1):
-        statements[i].grid(row=i+6, column=1, columnspan=3)
-        reasons[i].grid(row=i+6, column=4, columnspan=3)
-        refs[i].grid(row=i+6, column=7)
+        statements[i].grid(row=i, column=1, columnspan=3)
+        reasons[i].grid(row=i, column=4, columnspan=3)
+        refs[i].grid(row=i, column=7)
 
 
 def remove():
     if len(line_nums) > 1:
         line_nums[-1].grid_forget()
-        statements[int(q.get()) - 1].grid_forget()
-        reasons[int(q.get()) - 1].grid_forget()
-        refs[int(q.get()) - 1].grid_forget()
+        statements[int(p1.get()) - 1].grid_forget()
+        reasons[int(p1.get()) - 1].grid_forget()
+        refs[int(p1.get()) - 1].grid_forget()
 
         line_nums.pop()
-        statements.pop(int(q.get()) - 1)
-        reasons.pop(int(q.get()) - 1)
-        refs.pop(int(q.get()) - 1)
+        statements.pop(int(p1.get()) - 1)
+        reasons.pop(int(p1.get()) - 1)
+        refs.pop(int(p1.get()) - 1)
 
     for i in range(0, len(statements), 1):
-        statements[i].grid(row=i+6, column=1, columnspan=3)
-        reasons[i].grid(row=i+6, column=4, columnspan=3)
-        refs[i].grid(row=i+6, column=7)
+        statements[i].grid(row=i, column=1, columnspan=3)
+        reasons[i].grid(row=i, column=4, columnspan=3)
+        refs[i].grid(row=i, column=7)
 
 
 def really_important():
     try:
         full = "\\begin{enumerate}\\setcounter{enumi}{"
-        full += str(int(p.get()) - 1) + "}\\item " + parse(e1.get()) + "\\\\" + parse(e2.get())
+        full += str(int(p2.get()) - 1) + "}\\item " + parse(e1.get()) + "\\\\" + parse(e2.get())
         full += "\\\\\\\\\\renewcommand{\\arraystretch}{1.5}%\n\\begin{tabular}{rp{5cm}|p{5cm}l}\\multicolumn{2}{l}{Statements}&\\multicolumn{2}{l}{Reasons}\\\\\\hline\n"
         for i in range(0, len(statements), 1):
             full += str(i+1) + ".&"
@@ -163,35 +170,44 @@ def really_important():
     except ValueError:
         tkMessageBox.showerror("Oops!", "Make sure all spin boxes have numbers only")
 
-d = Button(tk, text="Copy LaTeX", command=really_important, width=10, height=1, background="#BFFFBF")
-d.grid(row=0, column=1)
+f1 = LabelFrame(tk, padx=10, pady=10, text="Options")
+f1.grid(row=0, column=0, columnspan=100)
 
-h = Button(tk, text="Save as", command=guide, width=10, height=1, background="#BFFFFF")
-h.grid(row=0, column=2)
+d = Button(f1, text="Copy LaTeX", command=really_important, width=10, height=1, background="#BFFFBF")
+d.grid(row=0, column=0, padx=5)
 
-j = Button(tk, text="Open", command=open_file, width=10, height=1, background="#BFFFFF")
-j.grid(row=0, column=3)
+h = Button(f1, text="Save as", command=guide, width=10, height=1, background="#BFFFFF")
+h.grid(row=0, column=1, padx=5)
 
-b1 = Button(tk, text="Insert end", command=insert, width=10, height=1, background="#FFFFBF")
-b1.grid(row=0, column=4)
+j = Button(f1, text="Open", command=open_file, width=10, height=1, background="#BFFFFF")
+j.grid(row=0, column=2, padx=5)
 
-b3 = Button(tk, text="Insert at", command=insert_at, width=10, height=1, background="#FFFFBF")
-b3.grid(row=0, column=5)
+b1 = Button(f1, text="Insert end", command=insert, width=10, height=1, background="#FFFFBF")
+b1.grid(row=0, column=3, padx=5)
 
-b2 = Button(tk, text="Remove at", command=remove, width=10, height=1, background="#FFBFBF")
-b2.grid(row=0, column=6)
+b3 = Button(f1, text="Insert at", command=insert_at, width=10, height=1, background="#FFFFBF")
+b3.grid(row=0, column=4, padx=5)
 
-q = Spinbox(tk, from_=1, to=1000, width=5)
-q.grid(row=0, column=7)
+b2 = Button(f1, text="Remove at", command=remove, width=10, height=1, background="#FFBFBF")
+b2.grid(row=0, column=5, padx=5)
 
-p = Spinbox(tk, from_=1, to=1000, width=5)
-p.grid(row=2, column=0)
+p1 = Spinbox(f1, from_=1, to=1000, width=5)
+p1.grid(row=0, column=6, padx=5)
 
-e1 = Entry(tk, width=115)
-e1.grid(row=2, column=1, columnspan=7)
+f2 = LabelFrame(tk, relief=FLAT, padx=10, pady=10)
+f2.grid(row=1, column=0, columnspan=100)
 
-e2 = Entry(tk, width=115)
-e2.grid(row=3, column=1, columnspan=7)
+p2 = Spinbox(f2, from_=1, to=1000, width=5)
+p2.grid(row=0, column=0)
+
+e1 = Entry(f2, width=100)
+e1.grid(row=0, column=1, columnspan=7)
+
+e2 = Entry(f2, width=100)
+e2.grid(row=1, column=1, columnspan=7)
+
+f3 = LabelFrame(tk, relief=FLAT, padx=10, pady=10)
+f3.grid(row=2, column=0, columnspan=100)
 
 
 def parse(par):
